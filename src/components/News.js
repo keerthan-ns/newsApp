5a0e77715c725006c2aa8a88b1c1df4c8163e9a8
Keerthan N S <keerthanns091@gmail.com>
2023-07-03 15:55:20 +0530

    [add]infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
-
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class News extends Component {
     static defaultProps={
@@ -22,8 +22,9 @@ export class News extends Component {
         super(props);
         this.state={
             articles: [],
-            loading: false,
+            loading: true,
             page:1,
+            totalResults:0,
         }
         document.title = `NewsGorilla- ${this.capitalize(this.props.category)}`;
     }
@@ -55,23 +56,43 @@ export class News extends Component {
         })
         this.updateNews();
     }
+    fetchMoreData=async ()=>{
+        this.setState({
+            page:this.setState.page+1
+        });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e7ee060733dd4af19a1a658279c90e6f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResults:parseData.totalResults,
+        })
+    }
   render() {
     return (
       <div className="container mt-20 mb-3 mx-auto">
         <h2 className="text-2xl md:text-3xl font-bold text-center">NewsGorilla - {this.capitalize(this.props.category)}</h2>
+        {this.state.loading && <Spinner/>}
+        <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length!==this.state.totalResults}
+            loader={<Spinner/>}
+        >
         {/* {this.state.loading && <Spinner/>} */}
-        <div className="my-4 mx-2 flex flex-wrap items-center justify-between">
-            {/* !this.state.loading && this.state.articles.map((element,i)=>{ */}
-            {
-                this.state.articles.map((element,i)=>{
-                    return <NewsItem key={i} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
-                })
-            }
-        </div>
-        <div className='container flex justify-center'>
+            <div className="my-4 mx-2 flex flex-wrap items-center justify-between">
+                {/* !this.state.loading && this.state.articles.map((element,i)=>{ */}
+                {
+                    this.state.articles.map((element,i)=>{
+                        return <NewsItem key={i} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+                    })
+                }
+            </div>
+        </InfiniteScroll>
+        {/* <div className='container flex justify-center'>
             <button disabled={this.state.page<=1} onClick={this.handlePrevClick} type="button" className={`${this.state.page<=1?"cursor-not-allowed dark:bg-gray-500 dark:hover:bg-gray-700":""} text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>&larr; Prev</button>
             <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} onClick={this.handleNextClick} type="button" className={`${this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)?"cursor-not-allowed dark:bg-gray-500 dark:hover:bg-gray-700":""} text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Next &rarr;</button>
-        </div>
+        </div> */}
       </div>
     )
   }
